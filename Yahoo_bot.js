d6a0c2b843e7c3ef4035b489c2d2f5753edb00e6
Maria Kadyrova <100441806+agishe@users.noreply.github.com>
2023-04-27 20:26:56 +0300

    Update Yahoo_bot.js

--- Yahoo_bot.js
+++ Yahoo_bot.js
@@ -1,1 +1,21 @@
+// ==UserScript==
+// @name         Bot for Yahoo
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Kadyrova Maria
+// @match        https://www.yahoo.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
 
+let keywords = ["Best PC games 2022: the must-play titles you don’t want to miss", "The 12 best beaches in Greece", "Moscow City Centre"];
+let keyword = keywords[getRandom(0, keywords.length)];
+let links = document.links;
+
+
+
+
+//https://www.techradar.com/news/best-pc-games
+//https://www.lonelyplanet.com/articles/best-beaches-greece
+//https://www.expedia.com/Car-Rentals-In-Moscow-City-Centre.d3000662241.Car-Rental-Guide
