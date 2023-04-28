807bbc891e9f6c058b8f1e4845f8431967850d49
Maria Kadyrova <100441806+agishe@users.noreply.github.com>
2023-04-28 17:10:27 +0300

    Update Bing_bot.js

--- Bing_bot.js
+++ Bing_bot.js
@@ -1,1 +1,32 @@
+// ==UserScript==
+// @name         Bot for Bing
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description
+// @author       Kadyrova Maria
+// @match        https://www.bing.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
 
+let keywords = ["10 самых популярных шрифтов от Google", "Отключение редакций и ревизий в WordPress", "Вывод произвольных типов записей и полей в WordPress"];
+let keyword = keywords[getRandom(0, keywords.length)];
+let links = document.links;
+let bingInput = document.getElementsByName("q")[0];
+let btn = document.getElementsByName("search")[0];
+
+if (btn != undefined) {
+  bingInput.value = keyword;
+  btn.click();
+} else {
+  for(let i = 0; i < links.length; i++) {
+    if (links[i].href.indexOf("napli.ru") != -1) {
+      let link = links[i];
+      console.log("Нашел строку " + link);
+      link.click();
+    }
+  }
+}
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
